c308092512b913e883b2db8341873db30efeeb55
anantsharrma <anantsharrmaa@gmail.com>
2025-11-03 21:17:12 +0530

    initialize

---
+++ assignment3 task 2.py
@@ -0,0 +1,11 @@
+import math
+def sqrt(n):
+    return math.sqrt(n)
+def log(n):
+    return math.log(n)
+def sin(n):
+    return math.sin(n)
+x = int(input("Enter the number: "))
+print(f"The square root of {x} is: {sqrt(x)}")
+print(f"The logarithm of {x} is: {log(x)}")
+print(f"The sin of {x} is: {sin(x)}")
